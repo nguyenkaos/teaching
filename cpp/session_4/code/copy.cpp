c9b1950f4aef1614213db6c7a47f723e10955704
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2015-12-14 23:54:06 +0100

    Modify wording

--- cpp/session_4/code/copy.cpp
+++ cpp/session_4/code/copy.cpp
@@ -2,14 +2,14 @@ vector::vector( vector const& src )
 {
   size_ = src.size();
   double* tmp = new double[src.size()];
-  std::copy(tmp,tmp+size, src.data());
+  std::copy(tmp, tmp+src.size(), src.data());
   data_ = tmp;
 }
 
 vector::vector( vector const& src )
 {
   unique_ptr<double[]> tmp = new double[src.size()];
-  std::copy(tmp.get(),tmp.get()+src.size(), src.data());
+  std::copy(tmp.get(), tmp.get()+src.size(), src.data());
 
   data_ = tmp.release();
   size_ = src.size();
