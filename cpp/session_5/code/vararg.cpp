cabf39369a1e7d946b22d0238bf56100ccb4d44c
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2016-01-04 14:13:38 +0100

    Fix some typos

--- cpp/session_5/code/vararg.cpp
+++ cpp/session_5/code/vararg.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
 
-int sum()
-{
-  return 0;
-}
+int sum() { return 0; }
 
 template<typename T0, typename... Ts>
 auto sum(T0 v0, Ts... vs)
 {
-  return v0+sum(vs...);
+  return v0 + sum(vs...);
 }
 
 int main()
 {
-  auto a = sum(1u,2,3.,4.5f);
-
+  auto a = sum(1u, 2, 3., 4.5f);
   std::cout << a << "\n";
-
   return 0;
 }
