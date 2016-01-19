47550b8a8f6d1f9e62498c5db8e76b5c8c0224b1
Guillaume Melquiond <guillaume.melquiond@inria.fr>
2016-01-19 08:54:02 +0100

    Fix some typos

--- cpp/session_7/code/generic/staticassert.cpp
+++ cpp/session_7/code/generic/staticassert.cpp
@@ -2,6 +2,6 @@
 
 template<typename T> T factorial(T n)
 {
-  static_assert( std::is_integral<T>::value, "factorial requires integral parameter");
-  return n<2 ? 1 : n*factorial(n-1);
+  static_assert(std::is_integral<T>::value, "factorial requires integral parameter");
+  return n < 2 ? 1 : n * factorial(n - 1);
 }
